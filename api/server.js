1325c32d31d98cb3d5f613317be4124fbcbdc0e2
Nick Perez <nick@Nicks-MacBook-Pro.local>
2020-05-18 20:44:51 -0700

    mvp

--- api/server.js
+++ api/server.js
@@ -4,19 +4,20 @@ const helmet = require('helmet');
 const Shoutouts = require('../data/shoutouts-model.js');
 
 const server = express();
+const db = require('../data/db.js')
 
 server.use(helmet());
 server.use(express.json());
 
-server.get('/', (req, res) => {
-  Shoutouts.find()
-  .then(shoutouts => {
-    res.status(200).json(shoutouts);
-  })
-  .catch (error => {
+server.get('/', async (req, res) => {
+  try {
+    const shoutouts = await db('shoutouts');
+    const messageOfTheDay = process.env.MOTD || 'Hello World!'; // add this line
+    res.status(200).json({ motd: messageOfTheDay, shoutouts }); // change this line
+  } catch (error) {
     console.error('\nERROR', error);
     res.status(500).json({ error: 'Cannot retrieve the shoutouts' });
-  });
+  }
 });
 
 server.post('/', (req, res) => {
